5700c23888af13ab3b6e8e2424bb9d16acdfbaa7
EC2 Default User <ec2-user@ip-172-31-46-223.ec2.internal>
2021-08-13 02:39:36 +0000

    Agregar regla https

--- sg.tf
+++ sg.tf
@@ -18,4 +18,12 @@ resource "aws_security_group" "http" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  
+  ingress {
+    description = "HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
