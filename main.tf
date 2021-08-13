6960143699fe53fe919300950ba09b8c49fdd978
EC2 Default User <ec2-user@ip-172-31-46-223.ec2.internal>
2021-08-13 02:10:59 +0000

    Cambiar nombre de org

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "CHANGE_ME!"
+    organization = "Konigsberg"
 
     workspaces {
       name = "tf-masterclass-demo"
